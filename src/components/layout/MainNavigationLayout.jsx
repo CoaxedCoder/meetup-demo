fdfc62be2fd8187e4adee2b8cd68f2d7b08fa983
CoaxedCoder <sreevishnu0495@gmail.com>
2021-09-19 09:42:15 +0530

    adding favorites badge

--- src/components/layout/MainNavigationLayout.jsx
+++ src/components/layout/MainNavigationLayout.jsx
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom";
-import classes from './MainNavigationLayout.module.css'
+import classes from "./MainNavigationLayout.module.css";
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
 
 export default function MainNavigationLayout() {
-    return (
-        <header className = {classes.header}>
-            <div className = {classes.logo}>
-                React Meetups
-            </div>
-            <nav>
-                <ul className>
-                    <li>
-                        <Link to = '/'>All Mettups</Link>
-                    </li>
-                      <li>
-                        <Link to = '/new-meetup'>New Mettup</Link>
-                    </li>
-                      <li>
-                        <Link to = '/favorites'>Favorites</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const favContext = useContext(FavoritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul className>
+          <li>
+            <Link to="/">All Mettups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Mettup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">
+              Favorites
+              <span className={classes.badge}>{favContext.totalFavorites}</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
